01165882d7aad73faa6a0c94eff137e60131ec79
Dzmitry Patseyuk <dzmitry_patseyuk@epam.com>
2016-04-19 16:14:18 +0300

    refactoring

--- pablic/client.js
+++ pablic/client.js
@@ -1,3 +1,17 @@
+// utils
+function createParam(object) {
+    var encodedString = '';
+    for (var prop in object) {
+        if (object.hasOwnProperty(prop)) {
+            if (encodedString.length > 0) {
+                encodedString += '&';
+            }
+            encodedString += encodeURI(prop + '=' + object[prop]);
+        }
+    }
+    return encodedString;
+};
+
 function Model() {}
 function Collection () {}
 
@@ -35,12 +49,15 @@ function Grid(className, height, width) {
   self.height = height;
   self.width = width;
   self.elements = [];
-  self.xhr = new XMLHttpRequest();
   self.channel = new Channel();
+};
 
-  var click = false;
-
+Grid.prototype.initialize = function () {
+  var self = this,
+      click = false;
   self.gridElement.onclick = function (e) {
+    e.stopPropagation();
+    e.preventDefault();
     if(click) return;
     click = !click;
     if(e.target.attributes.key) {
@@ -68,6 +85,7 @@ function Grid(className, height, width) {
       click = !click;
     }, 1000);
   });
+
   self.channel.on('incorrect', function (data) {
     if(self.height !== 3 && self.width !== 3) {
       self.height--;
@@ -83,72 +101,80 @@ function Grid(className, height, width) {
   });
 };
 
-// utils
-function createParam(object) {
-    var encodedString = '';
-    for (var prop in object) {
-        if (object.hasOwnProperty(prop)) {
-            if (encodedString.length > 0) {
-                encodedString += '&';
-            }
-            encodedString += encodeURI(prop + '=' + object[prop]);
-        }
-    }
-    return encodedString;
-};
-
 Grid.prototype.createCell = function (key) {
   var cell = document.createElement('img');
       self = this;
   cell.classList.add('cell');
   return {
-          ell: cell,
-          key: key
+          ell: cell
         };
 };
 
-Grid.prototype.build = function (height, width) {
-  var self = this;
+Grid.prototype.addedNodes = function (length, size) {
+  for (var i = 0; i < size - length; i++) {
+    var cell = this.createCell(i + length);
+    cell.ell.setAttribute('style', 'display: none;');
+    this.elements.push(cell);
+    this.gridElement.appendChild(cell.ell);
+  }
+};
 
-  this.getNewIcons({ count: height * width }, function (data) {
-    var sizeStyle = 'width: ' + width * 45 + 'px;' +
-                    'height:' + height * 45 + 'px;';
-    self.gridElement.setAttribute('style', sizeStyle);
-    self.gridElement.innerHTML = '';
-    // need clear elements correct
-    self.elements = [];
-    self.correctAnswer = data.correctAnswer;
-    for (var i = 0; i < data.randIconArr.length; i++) {
-      var cell = self.createCell(i);
-      cell.ell.setAttribute('src', data.randIconArr[i]);
-      cell.ell.setAttribute('key', i);
-      self.gridElement.appendChild(cell.ell);
-      self.elements.push(cell)
+Grid.prototype.reduceNodes = function (length, size) {
+  for (var i = length - 1; i >= size; i--) {
+    var node = this.elements[i].ell;
+    this.gridElement.removeChild(node);
+  }
+  this.elements = this.elements.slice(0, size);
+};
+
+Grid.prototype.updateCellImg = function (randIconArr) {
+  for (var i = 0; i < randIconArr.length; i++) {
+    self.elements[i].ell.classList.remove('correct');
+    self.elements[i].ell.classList.remove('incorrect');
+    self.elements[i].ell.removeAttribute('style');
+    self.elements[i].ell.setAttribute('src', randIconArr[i]);
+    self.elements[i].ell.setAttribute('key', i);
+  }
+}
+
+Grid.prototype.build = function () {
+  var self = this;
+  this.getNewIcons({ count: self.height * self.width }, function (data) {
+    var sizeStyle = 'width: ' + self.width * 45 + 'px;' +
+                    'height:' + self.height * 45 + 'px;',
+        size = self.height * self.width,
+        length = self.elements.length;
+
+    if(length < size) {
+      self.addedNodes(length, size);
+    } else {
+      self.reduceNodes(length, size);
     }
-  })
+
+    self.correctAnswer = data.correctAnswer;
+    self.gridElement.setAttribute('style', sizeStyle);
+    setTimeout(function () { self.updateCellImg(data.randIconArr); }, 1000);
+  });
 };
 
 Grid.prototype.getNewIcons = function (reqObj, cb) {
   var queryString = createParam(reqObj),
-      self = this;
-  self.xhr.open('GET', '/icons' + '?' + queryString);
+      self = this,
+      xhr = new XMLHttpRequest();
 
-  self.xhr.onreadystatechange = function (data) {
-    if(data.target.responseText) {
+  xhr.open('GET', '/icons' + '?' + queryString);
+  xhr.onreadystatechange = function (data) {
+    if (this.readyState === 4 && this.status === 200)
+    {
       var respond = JSON.parse(data.target.responseText);
       cb(respond);
     }
   }
-  self.xhr.send();
+  xhr.send();
 };
 
 window.onload = function () {
   var gridInstance = new Grid('grid', 3, 3);
-  gridInstance.build(gridInstance.height, gridInstance.width);
-
-  var relod = document.querySelector('#relod');
-  relod.onclick = function () {
-    gridInstance.build(gridInstance.height, gridInstance.width);
-  };
-
+  gridInstance.initialize();
+  gridInstance.build();
 };
