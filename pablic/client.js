6a5a6a42d07a5586b6228f65f1fccad3ed05da54
Dzmitry Patseyuk <dzmitry_patseyuk@epam.com>
2016-04-19 12:05:52 +0300

    made animation, added some style

--- pablic/client.js
+++ pablic/client.js
@@ -1,17 +1,87 @@
-function Grid(className) {
-  className = className[0] === '.' ? className : '.' + className;
-  this.gridElement = document.querySelector(className);
-  this.height = 0;
-  this.width = 0;
-  this.elements = [];
-  this.xhr = new XMLHttpRequest();
-};
-
 function Model() {}
 function Collection () {}
 
 function ModelGridItem() {}
 function CollectionGridItems () {}
+
+function Channel () {
+  this.events = {};
+}
+
+Channel.prototype.on = function (name, cb) {
+  if(!this.events[name]) {
+    this.events[name] = [cb];
+  } else {
+    this.events[name].push(cb);
+  }
+};
+
+Channel.prototype.emit = function (name, data) {
+  if(this.events[name]) {
+    var callbacks = this.events[name];
+    for (var i = 0; i < callbacks.length; i++) {
+      callbacks[i](data);
+    }
+  } else {
+    console.warn('event', name, 'not subscribed yet!');
+  }
+};
+
+function Grid(className, height, width) {
+  var self = this;
+  className = className[0] === '.' ? className : '.' + className;
+
+  self.gridElement = document.querySelector(className);
+  self.height = height;
+  self.width = width;
+  self.elements = [];
+  self.xhr = new XMLHttpRequest();
+  self.channel = new Channel();
+
+  var click = false;
+
+  self.gridElement.onclick = function (e) {
+    if(click) return;
+    click = !click;
+    if(e.target.attributes.key) {
+        var key = parseInt(e.target.attributes.key.nodeValue);
+        if(key === self.correctAnswer[0] || key === self.correctAnswer[1]) {
+          self.channel.emit('correct', self.correctAnswer);
+        } else {
+          self.channel.emit('incorrect', {
+            answer: key,
+            correctAnswer: self.correctAnswer
+          });
+        }
+    }
+   };
+
+  self.channel.on('correct', function (data) {
+    if(self.height !== 10 && self.width !== 10) {
+      self.height++;
+      self.width++;
+    }
+    self.elements[data[0]].ell.classList.add('correct');
+    self.elements[data[1]].ell.classList.add('correct');
+    setTimeout(function () {
+      self.build(self.height,self.width);
+      click = !click;
+    }, 1000);
+  });
+  self.channel.on('incorrect', function (data) {
+    if(self.height !== 3 && self.width !== 3) {
+      self.height--;
+      self.width--;
+    }
+    self.elements[data.answer].ell.classList.add('incorrect');
+    self.elements[data.correctAnswer[0]].ell.classList.add('correct');
+    self.elements[data.correctAnswer[1]].ell.classList.add('correct');
+    setTimeout(function () {
+      self.build(self.height,self.width);
+      click = !click;
+    }, 1000);
+  });
+};
 
 // utils
 function createParam(object) {
@@ -28,13 +98,11 @@ function createParam(object) {
 };
 
 Grid.prototype.createCell = function (key) {
-  var cell = document.createElement('div'),
-      img = document.createElement('img');
-  cell.appendChild(img);
+  var cell = document.createElement('img');
+      self = this;
   cell.classList.add('cell');
   return {
           ell: cell,
-          img: img,
           key: key
         };
 };
@@ -52,8 +120,8 @@ Grid.prototype.build = function (height, width) {
     self.correctAnswer = data.correctAnswer;
     for (var i = 0; i < data.randIconArr.length; i++) {
       var cell = self.createCell(i);
-      cell.img.setAttribute('src', data.randIconArr[i]);
-      cell.img.setAttribute('key', i);
+      cell.ell.setAttribute('src', data.randIconArr[i]);
+      cell.ell.setAttribute('key', i);
       self.gridElement.appendChild(cell.ell);
       self.elements.push(cell)
     }
@@ -61,21 +129,26 @@ Grid.prototype.build = function (height, width) {
 };
 
 Grid.prototype.getNewIcons = function (reqObj, cb) {
-  var queryString = createParam(reqObj);
-  this.xhr.open('GET', '/icons' + '?' + queryString);
-  this.xhr.onreadystatechange = function (data) {
-    cb(JSON.parse(data.target.responseText));
+  var queryString = createParam(reqObj),
+      self = this;
+  self.xhr.open('GET', '/icons' + '?' + queryString);
+
+  self.xhr.onreadystatechange = function (data) {
+    if(data.target.responseText) {
+      var respond = JSON.parse(data.target.responseText);
+      cb(respond);
+    }
   }
-  this.xhr.send();
+  self.xhr.send();
 };
 
 window.onload = function () {
-  var gridInstance = new Grid('grid');
-  gridInstance.build(3,3);
+  var gridInstance = new Grid('grid', 3, 3);
+  gridInstance.build(gridInstance.height, gridInstance.width);
 
   var relod = document.querySelector('#relod');
   relod.onclick = function () {
-    gridInstance.build(3,3);
+    gridInstance.build(gridInstance.height, gridInstance.width);
   };
 
 };
